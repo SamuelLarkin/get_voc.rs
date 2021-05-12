8d59e04c9604d4fb5fa3441af5cca6c65a71ec75
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2021-05-12 15:44:04 -0400

    [WIP] Folding multiple techniques into the same framework to do profiling.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,49 @@
 // cargo build --release
 // ~/.cargo/bin/cargo build --release --target x86_64-unknown-linux-musl
 
+/*
+~/.cargo/bin/hyperfine \
+    --prepare="cat $input > /dev/null" \
+    "target/release/get_voc.1 $input &> /dev/null" \
+    "target/release/get_voc.2 $input &> /dev/null" \
+    "target/x86_64-unknown-linux-musl/release/get_voc.1 $input &> /dev/null" \
+    "target/x86_64-unknown-linux-musl/release/get_voc.2 $input &> /dev/null"
+Benchmark #1: target/release/get_voc.1 /space/project/portage/models/WMT2020/hsb-de/corpora/preprocessing.multilingual/bpe.02k/DGT.cs-de.de &> /dev/null
+  Time (mean ± σ):     21.224 s ±  0.231 s    [User: 20.669 s, System: 0.541 s]
+  Range (min … max):   21.039 s … 21.680 s    10 runs
+
+Benchmark #2: target/release/get_voc.2 /space/project/portage/models/WMT2020/hsb-de/corpora/preprocessing.multilingual/bpe.02k/DGT.cs-de.de &> /dev/null
+  Time (mean ± σ):     39.253 s ±  1.103 s    [User: 38.582 s, System: 0.654 s]
+  Range (min … max):   38.584 s … 42.249 s    10 runs
+
+Benchmark #3: target/x86_64-unknown-linux-musl/release/get_voc.1 /space/project/portage/models/WMT2020/hsb-de/corpora/preprocessing.multilingual/bpe.02k/DGT.cs-de.de &> /dev/null
+  Time (mean ± σ):     35.875 s ±  0.093 s    [User: 35.355 s, System: 0.488 s]
+  Range (min … max):   35.755 s … 36.001 s    10 runs
+
+Benchmark #4: target/x86_64-unknown-linux-musl/release/get_voc.2 /space/project/portage/models/WMT2020/hsb-de/corpora/preprocessing.multilingual/bpe.02k/DGT.cs-de.de &> /dev/null
+  Time (mean ± σ):     70.884 s ±  0.122 s    [User: 70.309 s, System: 0.554 s]
+  Range (min … max):   70.715 s … 71.063 s    10 runs
+
+Summary
+  'target/release/get_voc.1 /space/project/portage/models/WMT2020/hsb-de/corpora/preprocessing.multilingual/bpe.02k/DGT.cs-de.de &> /dev/null' ran
+    1.69 ± 0.02 times faster than 'target/x86_64-unknown-linux-musl/release/get_voc.1 /space/project/portage/models/WMT2020/hsb-de/corpora/preprocessing.multilingual/bpe.02k/DGT.cs-de.de &> /dev/null'
+    1.85 ± 0.06 times faster than 'target/release/get_voc.2 /space/project/portage/models/WMT2020/hsb-de/corpora/preprocessing.multilingual/bpe.02k/DGT.cs-de.de &> /dev/null'
+    3.34 ± 0.04 times faster than 'target/x86_64-unknown-linux-musl/release/get_voc.2 /space/project/portage/models/WMT2020/hsb-de/corpora/preprocessing.multilingual/bpe.02k/DGT.cs-de.de &> /dev/null'
+*/
+
+/*
+ * [source](https://stackoverflow.com/questions/34733811/what-is-the-difference-between-iter-and-into-iter)
+ *     The iterator returned by into_iter may yield any of T, &T or &mut T, depending on the
+ *     context.
+ *     The iterator returned by iter will yield &T, by convention.
+ *     The iterator returned
+ *     by iter_mut will yield &mut T, by convention.
+ *
+ */
+
+// [](https://users.rust-lang.org/t/need-help-to-convert-example-to-rust/2658/2)
+
+
 use std::cmp::Reverse;
 use std::collections::BTreeMap;
 use std::collections::HashMap;
@@ -13,6 +56,12 @@ use std::fs;
 use std::io::{self, BufRead};
 use std::path::Path;
 use std::str::FromStr;
+ 
+extern crate regex;
+use regex::Regex;
+
+
+type CountMap = BTreeMap<String, u32>;
 
 
 
@@ -26,7 +75,7 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 
 
 
-fn word_count(filename: &String) -> HashMap<String, u32> {
+fn word_count1(filename: &String) -> HashMap<String, u32> {
     //let filename = "/space/project/portage/models/WMT2020/hsb-de/corpora/preprocessing.multilingual/bpe.02k/teacher.all/train.cs-de.de";
     let mut counts : HashMap<String, u32> = HashMap::new();
     //let mut counts : BTreeMap<String, u32> = BTreeMap::new();
@@ -53,6 +102,61 @@ fn word_count(filename: &String) -> HashMap<String, u32> {
 
 
 fn word_count2(filename: &String) -> HashMap<String, u32> {
+    let file = File::open(&filename).unwrap();
+    let rdr = io::BufReader::new(file);
+    //let mut mapped : BTreeMap<String, u32> = BTreeMap::new();
+    let mut mapped : HashMap<String, u32> = HashMap::new();
+    for line in rdr.lines() {
+        match line {
+            Ok(line_) => {
+                for token in line_.split(char::is_whitespace) {
+                    // Filter out multiple spaces delimiting to empty strings.
+                    if token.len() > 0 {
+                        *mapped.entry(token.to_owned()).or_insert(0) += 1;
+                    }
+                }
+            }
+            Err(e) => { 
+                println!("Error reading file: {}", e);
+                panic!("Error!");
+            }
+        }
+    }
+    mapped
+}
+
+
+
+fn word_count3(filename: &String) -> HashMap<String, u32> {
+    let file = File::open(filename);
+    let counts :HashMap<String, u32> = io::BufReader::new(file.unwrap())
+        // BufRead.lines().  The iterator returned from this function will yield instances of io::Result<String>.
+        .lines()
+
+        // Result.unwrap().  Returns the contained Ok value, consuming the self value.
+        .map(|r: io::Result<String>| r.unwrap())
+
+        // Iterator.flat_map().  Creates an iterator that works like map, but flattens nested structure.
+        .flat_map(|l: String| {
+            // pub fn String::split<'a, P>(&'a self, pat: P) -> Split<'a, P>
+            l.split(char::is_whitespace)
+                .map(String::from)
+                .collect::<Vec<_>>()
+        })
+
+        .fold(HashMap::new(), |mut counts: HashMap<String, u32>, word: String| {
+            *counts.entry(word.to_string()).or_insert(0) += 1;
+            counts
+        });
+
+    //println!("{:?}", counts);
+
+    counts
+}
+
+
+
+fn word_count4(filename: &String) -> HashMap<String, u32> {
     //let mut counts : HashMap<String, u32> = HashMap::new();
 
     let file = File::open(filename);
@@ -84,7 +188,9 @@ fn word_count2(filename: &String) -> HashMap<String, u32> {
         //.cloned()
 
         // Iterator.flat_map().  Creates an iterator that works like map, but flattens nested structure.
-        // String.split_whitespace().  Splits a string slice by whitespace.  The iterator returned will return string slices that are sub-slices of the original string slice, separated by any amount of whitespace.
+        // String.split_whitespace().  Splits a string slice by whitespace.  The iterator returned
+        // will return string slices that are sub-slices of the original string slice, separated by
+        // any amount of whitespace.
         //.flat_map(|l: String| l.split_whitespace())
         //.map(str::to_owned)
 
@@ -102,13 +208,53 @@ fn word_count2(filename: &String) -> HashMap<String, u32> {
 
 
 
+//[source](http://rosettacode.org/wiki/Word_frequency#Rust)
+fn word_count5(filename: &String) -> HashMap<String, u32> {
+    let word_regex = Regex::new("(?i)[^ ]+").unwrap();
+
+    let mut words : HashMap<String, u32> = HashMap::new();
+    let file = File::open(filename).unwrap();
+    for line in io::BufReader::new(file).lines() {
+        word_regex
+            .find_iter(&line.expect("Read error"))
+            .map(|m| m.as_str())
+            .for_each(|word| {
+                *words.entry(word.to_lowercase()).or_insert(0) += 1;
+            });
+    }
+
+    words
+}
+
+
+
+fn print_counts<I, K, V>(counts : I) where
+    I: Iterator<Item=(K, V)>,
+    K: ::std::fmt::Display,
+    V: ::std::fmt::Display,
+{
+    for (key, value) in counts {
+        println!("{}\t{}", key, value);
+    }
+}
+
+
+
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
+    let method_id = &args[1].parse::<i32>().unwrap();
+    let filename  = &args[2];
 
-    let counts = word_count2(filename);
+    let counts = match method_id {
+        1 => word_count1(filename),
+        2 => word_count2(filename),
+        3 => word_count3(filename),
+        4 => word_count4(filename),
+        5 => word_count5(filename),
+        _ => panic!("Unknown method id: {}", method_id),
+    };
 
     //let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
@@ -119,11 +265,19 @@ fn main() {
     */
 
     // [Word Frequency](http://rosettacode.org/wiki/Word_frequency#Rust)
-    let n = 10;
     let mut words: Vec<_> = counts.iter().collect();
     words.sort_unstable_by_key(|&(word, count)| (Reverse(count), word));
          
+    /*
+    let n = 10;
     for (word, count) in words.iter().take(n) {
         println!("{:8} {:>8}", word, count);
     }
+    */
+    /*
+    for (word, count) in words.iter() {
+        println!("{:8} {:>8}", word, count);
+    }
+    */
+    print_counts(words.into_iter().take(10));
 }
