dea6bb3449eb420bde51a0cd779bfa52bc09c269
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2023-11-10 17:07:50 -0500

    chore: Updated hyperfine

--- benches/hyperfine.sh
+++ benches/hyperfine.sh
@@ -6,22 +6,23 @@
 readonly corpus=${1:-corpus.small}
 
 which get_voc
-cargo build --release
 
+readonly get_voc_rs="cargo run --release -- -s"
+#readonly get_voc_rs="./target/release/get_voc -s"
 declare -ar tests=(
+   "$get_voc_rs wcff $corpus &> /dev/null"
+   "$get_voc_rs wcwf $corpus &> /dev/null"
+   "$get_voc_rs wcbwf $corpus &> /dev/null"
+   "$get_voc_rs wcfmf $corpus &> /dev/null"
+   "$get_voc_rs wcrf $corpus &> /dev/null"
+   "$get_voc_rs wc-f1 $corpus &> /dev/null"
+   "$get_voc_rs wc-f2 $corpus &> /dev/null"
+   "$get_voc_rs wc-f3 $corpus &> /dev/null"
+   "$get_voc_rs wc-f4 $corpus &> /dev/null"
+   "$get_voc_rs wc-f5 $corpus &> /dev/null"
+   "$get_voc_rs wc-f6 $corpus &> /dev/null"
    "get_voc -s $corpus &> /dev/null"
    "./get_voc.py < $corpus &> /dev/null"
-   "cargo run --release wc1 $corpus &> /dev/null"
-   "cargo run --release wc1a $corpus &> /dev/null"
-   "cargo run --release wc1b $corpus &> /dev/null"
-   "cargo run --release wc2 $corpus &> /dev/null"
-   "cargo run --release wc3 $corpus &> /dev/null"
-   "cargo run --release wc-f1 $corpus &> /dev/null"
-   "cargo run --release wc-f2 $corpus &> /dev/null"
-   "cargo run --release wc-f3 $corpus &> /dev/null"
-   "cargo run --release wc-f4 $corpus &> /dev/null"
-   "cargo run --release wc-f5 $corpus &> /dev/null"
-   "cargo run --release wc-f6 $corpus &> /dev/null"
 )
 
 
@@ -29,10 +30,11 @@ function run {
    cat src/main.rs
 
    hyperfine \
+      --setup="cargo build --release" \
       --shell bash \
-      --style full \
       --export-json hyperfine.json \
       --prepare="cat $corpus > /dev/null" \
+      --style full \
       "${tests[@]}"
 }
 
